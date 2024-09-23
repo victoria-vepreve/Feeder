e38a5eb98c7b68be6e62717b6680d39c9c651445
victoria-vepreve <victoriav88@nycstudents.net>
2024-09-23 09:30:22 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,51 +1,14 @@
-public class Feeder {
-    public Feeder() {}
-    /**
-     * The amount of food, in grams, currently in the bird feeder; initialized
-     * in the constructor and always greater than or equal to zero
-     */
-
-
-    public int currentFood;
-    
-    /**
-     * Simulates one day with numBirds birds or possibly a bear at the bird
-     * feeder, as described in part (a) Precondition: numBirds > 0
-     */
-    public void simulateOneDay(int numBirds)
+public class Main
+{
+    public static void main(String[] args)
     {
-        if(Math.random() < 0.05) currentFood = 0;
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
 
-        else
-        {
-            int eaten = (int) (Math.random() * 41) + 10;
-            currentFood -= eaten * numBirds;
-            if (currentFood < 0) currentFood = 0;
-        }
-    }
+        Feeder j = new Feeder(2400);
+        System.out.println(j.simulateManyDays(10, 4));
+        
 
-    /**
-     * Returns the number of days birds or a bear found food to eat at the
-     * feeder in this simulation, as described in part (b) Preconditions:
-     * numBirds > 0, numDays > 0
-     */
-    public int simulateManyDays(int numBirds, int numDays) 
-    {
-        int days = 0;
-        return 0;
-    }
-
-    public int getCurrentFood()
-        {
-            return currentFood;
-        }
-    
-    public Feeder(int c)
-    {
-        currentFood = c;
-    }
-    public String toString()
-    {
-        return "Current Food: " + currentFood;
     }
 }
