edc5b057d0d3a917d20fe82a2a921c6bb4a84e84
victoria-vepreve <victoriav88@nycstudents.net>
2024-09-25 09:24:05 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,13 +2,32 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder i = new Feeder(100);
-        i.simulateOneDay(5);
-        System.out.println(i);
+        // Test Case 1
+        Feeder a = new Feeder(500);
+        a.simulateOneDay(12);
+        System.out.println(a);
 
-        Feeder j = new Feeder(2400);
-        System.out.println(j.simulateManyDays(10, 4));
+        //Test Case 2
+        Feeder b = new Feeder(1000);
+        b.simulateOneDay(22);
         
+        //Test Case 3
+        Feeder c = new Feeder(100);
+        c.simulateOneDay(5);
+
+        //Test Case 4
+        Feeder d = new Feeder(2400);
+        d.simulateManyDays(10,4);
+        System.out.println(d);
+
+        //Test Case 5
+        Feeder e = new Feeder(250);
+        e.simulateManyDays(10, 5);
+
+        //Test Case 6
+        Feeder f = new Feeder(0);
+        f.simulateManyDays(5, 10);
+
 
     }
 }
